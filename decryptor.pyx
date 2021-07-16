80907679c1ccd942af94bfab1975cdb1385bf6e5
Victor Shilin <chrono.monochrome@gmail.com>
2021-07-16 16:49:36 +0300

    cythonize extract()

--- decryptor.pyx
+++ decryptor.pyx
@@ -1,4 +1,3 @@
-# distutils: language = c++
 # cython: language_level=3
 "RPG Maker VX ace games decryptor - loosely based on RGSSAD-RGSS2A-RGSS3A Decryptor"
 
@@ -98,66 +97,138 @@ cdef class BinaryReader:
 			raise
 		self.file.close()
 		return self
+		
+from libc.stdlib cimport malloc, free
+from libc.string cimport strcpy, memcpy, memset, strlen
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+import numpy
 
-class File:
-	def __init__(self, sPath, iSize, iOffset, iKey):
+from libc.stdio cimport *
+
+cdef class File:
+	cdef object path;
+	cdef unsigned int size, offset, key;
+	cdef unsigned char *filedata;
+
+	def __cinit__(self, object sPath, unsigned int iSize, unsigned int iOffset, unsigned int iKey):
 		# replace backslashes in a file name with a common slash
-		self.path = sPath.replace(b"\\", b"/")
+		self.path = sPath
 		self.size = iSize
 		self.offset = iOffset
 		self.key = iKey
 		
-	def DecryptFileData(self, sRGSSFilePath):
-		fDecrypt = []
-		iKey = self.key
-		tempKey = getBytes(iKey)[::-1]
-		j = 0
+	# cdef void readData(self, char *sRGSSFilePath, char **filedata):
+		# cdef FILE *f;
+		# #ptr_fr = fopen(sRGSSFilePath, "rb")
+		# f = fopen(sRGSSFilePath, "rb")
+		# #f.seek(self.offset)
+		# #memcpy(filedata, <void*>f.read(self.size), self.size)
+		# #print(strlen(filedata), self.size)
+		# fread(&filedata, 1, self.size, f)
+		# #print(self.size, len(filedata))
+		# fclose(f)
+		# #f.close()
 		
-		f = open(sRGSSFilePath, "rb")
-		f.seek(self.offset)
-		bFileData = f.read(self.size)
-		f.close()
-		for b in bFileData:
+	cdef public unsigned char *DecryptFileData(self, char *sRGSSFilePath):
+		#cdef char *fDecrypt = <char *> malloc(self.size * sizeof(char));
+		cdef unsigned char *fDecrypt = <unsigned char *> PyMem_Malloc(self.size * sizeof(unsigned char));
+		cdef unsigned char *filedata = <unsigned char *> PyMem_Malloc(self.size * sizeof(unsigned char));
+		
+		memset(fDecrypt, 0, self.size * sizeof(unsigned char))
+		memset(filedata, 0, self.size * sizeof(unsigned char))
+		
+		
+		#cdef char *tempKey = <char *> PyMem_Malloc(4 * sizeof(char));
+		#fDecrypt = numpy.zeros(self.size)
+		#cdef unsigned char fDecrypt[65535];
+		
+		cdef unsigned int iKey = self.key;
+		#print("before getBytes")
+		cdef list tempKey;
+		tempKey = getBytes(iKey)
+		#return "test"
+		#print("after getBytes = %s" % str(tempKey))
+		#strcpy(tempKey, getBytes(iKey));
+		cdef int i, j = 0;
+		cdef unsigned char b;
+
+		cdef FILE* cfile
+		cfile = fopen(sRGSSFilePath, "rb")
+		fseek(cfile, self.offset, 1)
+		fread(filedata, 1, self.size, cfile)
+		fclose(cfile)
+		
+		#self.readData(sRGSSFilePath, &filedata)
+		#print(filedata)
+
+		for i, b in enumerate(filedata):
 			if (j == 4):
 				j = 0
 				iKey = next_iKey(iKey)
-				tempKey = getBytes(iKey)[::-1]
-			fDecrypt.append(b ^ tempKey[j])
+				#print("before getBytes (%d)" % i)
+				tempKey = getBytes(iKey)
+				#print("after getBytes (%d) = %s" % (i, str(tempKey)))
+			#fDecrypt[i] = b ^ tempKey[j]
+			fDecrypt[i] = b
 			j += 1
+			
+		#self.filedata = fDecrypt
+		self.filedata = fDecrypt
+		#cdef FILE* cfile
+		cfile = fopen(self.path, "wb")
+		fwrite(<void*>self.filedata, 1, self.size, cfile)
+		fclose(cfile)
+		#PyMem_Free(filedata)
+		#PyMem_Free(tempKey)
 		
 		return fDecrypt
 
-	def extract(self, sRGSSFilePath, targetDir, suppress_output = False):
+	cpdef public void extract(self, char *sRGSSFilePath, char *targetDir, suppress_output):
 		"Extract a given file from the given RGSSAD file (sRGSSFilePath) to targetDir"
 		
 		basedir = os.path.join(targetDir, os.path.split(self.path)[0])
 		if not os.path.exists(basedir):
 			os.makedirs(basedir)
 
-		if not suppress_output:
-			f = open(os.path.join(targetDir, self.path), "wb")
+		#if not suppress_output:
+		#	f = open(os.path.join(targetDir, self.path), "wb")
 
-		filedata = self.DecryptFileData(sRGSSFilePath)
+		debug_print("before DecryptFileData")
 
-		if not suppress_output:
-			f.write(bytes(filedata))
-			f.close()
+		cdef unsigned char *filedata = self.DecryptFileData(sRGSSFilePath)
+		return
+		#output = array.array("B", filedata).tobytes()
+		#print(filedata)
+		cdef FILE* cfile
+		cfile = fopen(self.path, "wb")
+		fwrite(<void*>self.filedata, 1, self.size, cfile)
+		fclose(cfile)
+
+		debug_print("write data")
 		
+		#if not suppress_output:
+		#f.write(output)
+		#f.close()
+		#free(filedata)
+		debug_print("free memmory")
+		PyMem_Free(self.filedata)
+
 def getBytes_reverse(iValue):
-	res = []
+	res = [];
 	nBits = 0
 	while nBits >= 0:
 		nBits += 8
 		res.insert((iValue >> nBits) & 0xff, 4)
 
 	return res
-	
+
 def getBytes(iValue, nBytes = 4):
-	res = []
+	res = [0] * 4;
 	nBits = nBytes * 8
-	while nBits > 0:
+	
+	for i in range(3, -1, -1):
 		nBits -= 8
-		res.append((iValue >> nBits) & 0xff)
+		res[i] = ((iValue >> nBits) & 0xff)
 				
 	return res
 
@@ -219,35 +290,7 @@ cpdef public object ReadRGSSADV1(char *sPath, unsigned int iKey, int max_count):
 				debug_print(" - end of file, last file_size = %d" % file_size)
 				break
 		return files
-		
-# def DecryptNameV3(bNameEnc, key):
-	# bNameDec = [];
-	# keyBytes = struct.pack("i", key)
-	# j = 0
 
-	# for b in bNameEnc:
-		# if (j == 4):
-			# j = 0
-		# bNameDec.append(ord(b) ^ ord(keyBytes[j]))
-		# j += 1
-
-	# return array.array("B", bNameDec).tostring()
-	
-# cdef object DecryptNameV3(bytes bNameEnc, unsigned int length, unsigned int key):
-	# cdef char bNameDec[255];
-	# cdef char b;
-	# cdef int j = 0;
-	# cdef int i = 0;
-
-	# for b in bNameEnc:
-		# if (j == 4):
-			# j = 0
-		# bNameDec[i] = b ^ ((key >> 8 * (4 - j)) & 0xff)
-		# j += 1
-		# i += 0
-
-	# return array.array("B", bNameDec[:length]).tostring()
-	
 cdef object DecryptNameV3(bytes bNameEnc, unsigned int length, unsigned int key):
 	cdef unsigned char bNameDec[255];
 	cdef char b;
@@ -263,7 +306,7 @@ cdef object DecryptNameV3(bytes bNameEnc, unsigned int length, unsigned int key)
 		j += 1
 		i += 1
 
-	return array.array("B", bNameDec[:length]).tostring()
+	return array.array("B", bNameDec[:length]).tobytes()
 
 cdef unsigned int keyV3 = 0
 
@@ -284,7 +327,7 @@ cpdef public object ReadRGSSADV3(char *sPath, int max_count):
 		iKey = br.read("uint32") * 9 + 3
 
 		while (max_count >= numFiles):
-			if (numFiles % 100) == 0: print(numFiles)
+			#if (numFiles % 100) == 0: print(numFiles)
 	
 			file_offset = br.read("uint32")
 			file_offset = DecryptIntV3(file_offset, iKey)
